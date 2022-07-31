6bb37b3898db5e7ddf0c783f90d73d6fefd7ba45
Cosmas Eric Septian <cosmas.eric.septian@polytron.co.id>
2022-07-31 11:40:05 +0700

    add EON SPI Flash for uboot add snapcast package rootfs_overlay for wifi, speaker-test start post-build.sh modification to strip down size wifi firmware linux_defconfig modification allwinner_suniv_defconfig modification

--- board/sunivboard/v1/post-build.sh
+++ board/sunivboard/v1/post-build.sh
@@ -3,3 +3,8 @@
 #	|| echo '/dev/ttyGS0::respawn:/sbin/getty -L  /dev/ttyGS0 0 vt100 # GADGET_SERIAL' >> "${TARGET_DIR}/etc/inittab"
 grep -q "ubi0:persist" "${TARGET_DIR}/etc/fstab" \
 	|| echo 'ubi0:persist	/root		ubifs	defaults	0	0' >> "${TARGET_DIR}/etc/fstab"
+
+# remove all content in target firmware/brcm
+# and copy specific file in overlay dir
+rm -rf "${TARGET_DIR}"/lib/firmware/brcm/*
+cp "${BR2_EXTERNAL_SUNIVBOARD_PATH}"/board/sunivboard/rootfs_overlay/lib/firmware/brcm/* "${TARGET_DIR}"/lib/firmware/brcm
