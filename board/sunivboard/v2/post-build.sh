949bdda4e72fb2046715df5626fc7c8a54c8a549
frame1bit <framexbit@gmail.com>
2022-10-15 08:20:12 +0700

    activate package MPD (Music Player Daemon) and update rootfs_overlay for MPD playlist contain playable url from NHK podcast. add delete command on post-build.sh to remove cypress firmware (currently not used for AP6212 wifi module) to save spi-flash memory

--- board/sunivboard/v2/post-build.sh
+++ board/sunivboard/v2/post-build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+!/bin/sh
 #grep -q "GADGET_SERIAL" "${TARGET_DIR}/etc/inittab" \
 #	|| echo '/dev/ttyGS0::respawn:/sbin/getty -L  /dev/ttyGS0 0 vt100 # GADGET_SERIAL' >> "${TARGET_DIR}/etc/inittab"
 # grep -q "ubi0:persist" "${TARGET_DIR}/etc/fstab" \
@@ -8,3 +8,8 @@
 # and copy specific file in overlay dir
 rm -rf "${TARGET_DIR}"/lib/firmware/brcm/*
 cp "${BR2_EXTERNAL_SUNIVBOARD_PATH}"/board/sunivboard/rootfs_overlay/lib/firmware/brcm/* "${TARGET_DIR}"/lib/firmware/brcm
+
+# remove cypress firmware directory
+# this firmware always selected if config BR2_PACKAGE_LINUX_FIRMWARE_BRCM_BCM43XX enabled
+# remove to save space in limited spi flash
+rm -rf "${TARGET_DIR}"/lib/firmware/cypress/*
