7110b6e20d3e2dca2694620102b1a423d01b6902
frame1bit <framexbit@gmail.com>
2022-10-17 06:46:38 +0700

    on progress.. set ir_input handler

--- package/ir_input/src/ir_input.c
+++ package/ir_input/src/ir_input.c
@@ -4,17 +4,103 @@
 #include <stdlib.h>
 #include <linux/input.h>
 #include <sys/select.h>
+#include <poll.h>
 
+
+unsigned char *device;
+
+struct irc
+{
+    __u16 code;
+    __u32 value; 
+    __u8  keyState;
+};
+
+#define KEY_PRESSED_JITTER  0
+#define KEY_PRESSED_ONCE    1
+#define KEY_PRESSED_REPEAT  2
+
+/**
+ * struct rc_map_table - represents a scancode/keycode pair
+ *
+ * @scancode: scan code (u32)
+ * @keycode: Linux input keycode
+ */
+struct rc_map_table {
+	__u32	scancode;
+	__u32	keycode;
+};
+
+static const __u16 key_map[][3] = {
+    {0x9117,    KEY_POWER,  KEY_POWER2},
+    {0x9115,    KEY_MUTE,   KEY_MICMUTE},
+    {}
+};
+
+__u16 scan_ir_code(int fd)
+{
+    struct irc ircode;
+    ssize_t bytesRead;
+    int ret;
+    fd_set readfds;
+    struct input_event event;
+    struct timeval timeout;
+
+    /* Wait on fd for input */
+    FD_ZERO(&readfds);
+    FD_SET(fd, &readfds);
+
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 250000;
+
+    if ( !(select(fd + 1, &readfds, NULL, NULL, &timeout)) )
+    {
+        ircode.value = 0;
+        return 0;
+    }
+
+    /* File descriptor is now ready */
+    if (FD_ISSET(fd, &readfds))
+    {
+        bytesRead = read(fd, &event, sizeof(struct input_event));
+        if (bytesRead > 0)
+        {
+            if (event.type == EV_KEY)
+            {
+                printf("[EV_KEY] code: %d value: 0x%X\n",event.code, event.value);
+            } 
+            else if (event.type == EV_MSC)
+            {
+                //printf("[EV_MSC] code: %d value: 0x%x \n", event.code, event.value);
+                if (ircode.value != event.value) {
+                    ircode.value = event.value;
+                    printf("Value: 0x%x \n", ircode.value);
+                    // for (int i = 0; i<3; i+=1) {
+                    //     if (ircode.value == key_map[i][0])
+                    //         return key_map[i][1];
+                    // } 
+                    return ircode.value;
+                }
+                // else 
+                // {
+                //     printf("Repeat: 0x%x \n", ircode.value);
+                //     /* repeat */
+                //     for (int i = 0; i<3; i+=1) {
+                //         if (ircode.value == key_map[i][0])
+                //             return key_map[i][2];
+                //     } 
+                // }
+            }
+        }
+    }
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
     int fd;
-    struct input_event event;
-    ssize_t bytesRead;
-    unsigned char *device;
-
-    int ret;
-    fd_set readfds;
+    __u16 code;
 
     if (argc < 2) {
         return -1;
@@ -22,7 +108,7 @@ int main(int argc, char **argv)
 
     device = argv[1];
 
-    fd = open(device, O_RDONLY);
+    fd = open(device, O_RDONLY | O_NONBLOCK);
     /* Let's open our input device */
     if (fd < 0)
     {
@@ -30,60 +116,11 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
+
     while (1)
     {
-        /* Wait on fd for input */
-        FD_ZERO(&readfds);
-        FD_SET(fd, &readfds);
-
-        ret = select(fd + 1, &readfds, NULL, NULL, NULL);
-
-        if (ret == -1)
-        {
-            fprintf(stderr, "select call on %s: an error occurred",
-                    device);
-            break;
-        }
-        else if (!ret)
-        { /* If we have decided to use timeout */
-            fprintf(stderr, "select on %s: TIMEOUT", device);
-            break;
-        }
-
-        /* File descriptor is now ready */
-        if (FD_ISSET(fd, &readfds))
-        {
-            bytesRead = read(fd, &event,
-                             sizeof(struct input_event));
-            if (bytesRead == -1)
-            {
-                /* Process read input error*/
-                fprintf(stderr, "read input error \n");
-            }
-            
-            if (bytesRead != sizeof(struct input_event))
-            {
-                /* Read value is not an input even */
-                printf("Read value is not an input event \n");
-            }
-            
-            if (event.type == EV_KEY)
-            {
-                /**
-                 * do parsing ...
-                */
-                printf("[EV_KEY] code: %d value: %d\n",event.code, event.value);
-            } 
-            else if (event.type == EV_MSC)
-            {
-                /*  
-                * do parsing ...
-                */
-                printf("[EV_MSC] code: %d value: %d \n", event.code, event.value);
-            }
-            else {
-                /** exception **/
-            }
+        if ((code = scan_ir_code(fd)) != 0) {
+            printf("Scan value: 0x%x\n", code);
         }
     }
     close(fd);
---
+++ package/ir_input/src/ir_input.h
@@ -0,0 +1,5 @@
+#ifndef IR_INPUT_H
+#define IR_INPUT_H
+
+
+#endif /*IR_INPUT_H*/
